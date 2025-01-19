d64117e4cd69f0b1473881e6d38a2c717b0df78e
Fateh-Aletomeh <fateh.aletomeh.24@ucl.ac.uk>
2025-01-19 14:47:17 +0000

    Added command parser and converting headers

--- main.py
+++ main.py
@@ -1,1 +1,57 @@
+import sys
+import re
+from typing import Optional
 
+
+class Tag:
+    def __init__(self, name: str, content: str) -> None:
+        self.name = name
+        self.html = f"<{name}>{content}</{name}>\n"
+
+
+def parseInput() -> Optional[tuple[str]]:
+    # Input should be in format: python main.py input.md output.html
+    if len(sys.argv) != 3:
+        print("Error: incorrect number of arguments")
+    elif sys.argv[1][-3:] != ".md":
+        print("Error: incorrect filetype for input file")
+    elif sys.argv[2][-5:] != ".html":
+        print("Error: incorrect filetype for output file")
+    else:
+        return (sys.argv[1], sys.argv[2])
+    
+    print("Usage: python main.py input.md output.html\n")
+    sys.exit(1)
+
+
+def parseLine(line: str) -> Optional[Tag]:
+    # Headers
+    if re.search("^# ", line):
+        return Tag("h1", line[2:].strip())
+    if re.search("^## ", line):
+        return Tag("h2", line[3:].strip())
+    if re.search("^### ", line):
+        return Tag("h3", line[4:].strip())
+    if re.search("^#### ", line):
+        return Tag("h4", line[5:].strip())
+
+
+def main() -> None:
+    input, output = parseInput()
+    in_file = open(input, 'r')
+    out_file = open(output, 'a')
+    out_file.write("<html>\n<header></header>\n<body>\n")
+
+    for line in in_file.readlines():
+        try:
+            out_file.write(parseLine(line).html)
+        except TypeError:
+            continue
+
+    out_file.write("</body>\n</html>")
+    out_file.close()
+    in_file.close()
+
+
+if __name__ == "__main__":
+    main()
